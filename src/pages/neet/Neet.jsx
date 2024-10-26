b218ba07ee8e5827a5016f98f49981ad5cce4cb7
Piyushxz <140832863+Piyushxz@users.noreply.github.com>
2024-10-26 14:20:17 +0530

    neet page cards

--- src/pages/neet/Neet.jsx
+++ src/pages/neet/Neet.jsx
@@ -23,7 +23,15 @@ const Neet = () => {
                     <img  className="w-full" src="https://res.cloudinary.com/dpzpn3dkw/image/upload/v1727444104/PLP_Ticker_tx7nvz.webp" />
             </div>
 
+
             <div className="w-full flex justify-center mt-20">
+                <div className="w-2/3 flex ">
+                    <h1 className="font-montserrat font-semibold text-3xl text-white py-6">Online Coaching for NEET</h1>
+                </div>
+            </div>
+
+            <div className="w-full flex justify-center ">
+
                 <div className="w-2/3 flex justify-center ">
                     <Card onClick={()=>navigate("/neet/online-coaching-class-11")}
                     title={"For 11th"} title2={"1 year programs"} img={"https://res.cloudinary.com/dpzpn3dkw/image/upload/v1729766512/U_11th_ah95gg.webp"} />
@@ -33,7 +41,87 @@ const Neet = () => {
                     title={"For 12th Plus"} title2={"1 year programs"} img={"https://res.cloudinary.com/dpzpn3dkw/image/upload/v1729766519/U_12th__bv4mhn.webp"} />
                 </div>
             </div>
-        
+
+            <div className="w-full flex justify-center mt-20">
+                <div className="w-2/3 flex ">
+                    <h1 className="font-montserrat font-semibold text-3xl text-white py-6">What’s special about our NEET Programs?</h1>
+                </div>
+            </div>
+
+            <div className="w-full flex justify-center mt-2 items-center">
+                <div style={{backgroundColor:"rgb(26, 36, 57)"}} className="w-2/3 flex h-64 rounded-lg">
+                    <div className="flex flex-row justify-center ">
+                        <div className="flex flex-col justify-center items-center p-10">
+                            
+                            <img className="w-40 h-40"src="https://res.cloudinary.com/dpzpn3dkw/image/upload/v1729268363/widget/stream_selection/live_intractive_class_lxfjvd.webp"/>
+                            <h1 style={{ color: "white" }} className="font-montserrat font-semibold text-xs pt-10 ">Daily Live Interactive Classes</h1>
+                            
+                          
+
+                        </div>
+                        <div className="flex flex-col justify-center items-center p-10">
+                            
+                            <img className="w-40 h-40"src="https://res.cloudinary.com/dpzpn3dkw/image/upload/v1729493739/widget/stream_selection/daily_doubt_solving_huffs4.webp"/>
+                            <h1 style={{ color: "white" }} className="font-montserrat font-semibold text-xs pt-10 ">Personalised Study Tools</h1>
+                            
+                          
+
+                        </div>
+                        <div className="flex flex-col justify-center items-center p-10">
+                            
+                            <img className="w-40 h-40"src="https://res.cloudinary.com/dpzpn3dkw/image/upload/v1729268363/widget/stream_selection/weekly_self_study_plan_hegxyx.webp"/>
+                            <h1 style={{ color: "white" }} className="font-montserrat font-semibold text-xs pt-10 ">Weekly Self-study Plan</h1>
+                            
+                          
+
+                        </div>
+                        <div className="flex flex-col justify-center items-center p-10">
+                            
+                            <img className="w-40 h-40"src="https://res.cloudinary.com/dpzpn3dkw/image/upload/v1729268362/widget/stream_selection/smart_test_taking_strategies_ouvab9.webp"/>
+                            <h1 style={{ color: "white" }} className="font-montserrat font-semibold text-xs pt-10 ">Score Improvement Strategy
+
+                                </h1>
+                            
+                          
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div className="w-full flex justify-center mt-20">
+                <div className="w-2/3 flex ">
+                    <h1 className="font-montserrat font-semibold text-3xl text-white py-6">Alternate Coaching you can explore</h1>
+                </div>
+            </div>
+            <div className="w-full flex justify-center ">
+                <div  className="w-2/3 h-64 flex justify-between ">
+                    <div style={{ backgroundColor: "rgb(26, 36, 57)" }} className="w-1/2 h-32  rounded-lg flex justify-between">
+                        <div className="flex jusitfy-center items-center">
+                            <img className="w-24 h-24 p-6"src="https://res.cloudinary.com/dpzpn3dkw/image/upload/v1729681260/distance_learning_vwq4zl.webp"/>
+                            <h1 className="font-montserrat font-semibold text-2xl text-white p-6">Distance Learning</h1>
+                        </div>
+                    </div>
+                    <div style={{ backgroundColor: "rgb(26, 36, 57)" }} className="w-1/2 h-32  rounded-lg flex justify-between ml-6">
+                        <div className="flex jusitfy-center items-center">
+                            <img className="w-24 h-24 p-6"src="https://res.cloudinary.com/dpzpn3dkw/image/upload/v1729681261/online_test_series_r6f0ms.webp"/>
+                            <h1 className="font-montserrat font-semibold text-2xl text-white p-6">Online Test Series</h1>
+                        </div>
+                    </div>
+
+
+
+                   
+                </div>
+            </div>
+
+
+            <div className="w-full flex justify-center mt-20">
+                <div className="w-2/3 flex ">
+                    <h1 className="font-montserrat font-semibold text-3xl text-white py-6">Looking for a classroom based Program?</h1>
+                </div>
+            </div>
+
         </>
     );
 }
