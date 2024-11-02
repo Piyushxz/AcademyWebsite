7ec73ed996cd6da5419f0625dc2c593ba254bf72
Piyushxz <140832863+Piyushxz@users.noreply.github.com>
2024-11-02 12:41:24 +0530

    neet page css fix

--- src/pages/neet/class12th.jsx
+++ src/pages/neet/class12th.jsx
@@ -7,7 +7,7 @@ const Class12th = () =>{
             <div className="w-full mt-24 flex justify-center">
                 <div className="w-4/5 flex justify-start">
                     <h2 className="font-montserrat font-semibold text-3xl text-white">
-                        NEET programs for Class 11th
+                        NEET programs for Class 12th
                     </h2>
                 </div>
             </div>
@@ -15,97 +15,121 @@ const Class12th = () =>{
             <HeaderAndCard imageUrl="https://res.cloudinary.com/dpzpn3dkw/image/upload/v1727084826/Live_oooh8u.webp" headerTitle="Live Online Program" type=" LIVE" cardTitle1="Fullstack Course 1.0" cardTitle2="All Subjects:LIVE" description1="4 months LIVE revision course" description2="Up to 20 online tests" description3="24/7 doubt resolution, group mentorship & more" price="19,990"/>
             <HeaderAndCard imageUrl="https://res.cloudinary.com/dpzpn3dkw/image/upload/v1727084835/Test_Series_m5smh4.webp" headerTitle="Recorded Lectures Program" type=" RECORDED" cardTitle1="Leader Recorded Lecture" cardTitle2="Video Lecture + Test Series" description1="Latest recordings covering full syllabus" description2="20+ All India Tests" description3="Exclusive ALLEN's Question Bank" price="14,999"/>
             <div className="w-full mt-24 flex justify-center flex-col items-center">
-                <div className="w-4/5 flex justify-between items-center border-b-2 relative">
-                    <h2 className="font-montserrat font-semibold text-2xl text-white">
-                    Online Test Series Packages
-                    </h2>
-                    <img 
-                        src="https://res.cloudinary.com/dpzpn3dkw/image/upload/v1727084760/Recorded_bohqhp.webp"
-                        alt="Recorded Lectures Program" 
-                        className="h-16 w-16 max-h-48 md:max-h-64 object-contain border-none" 
-                        loading="eager" 
-                        draggable="false"
-                    />
+    <div className="w-11/12 md:w-4/5 flex justify-between items-center border-b-2 relative">
+        <h2 className="font-montserrat font-semibold text-2xl text-white">
+            Online Test Series Packages
+        </h2>
+        <img 
+            src="https://res.cloudinary.com/dpzpn3dkw/image/upload/v1727084760/Recorded_bohqhp.webp"
+            alt="Recorded Lectures Program" 
+            className="h-16 w-16 max-h-48 md:max-h-64 object-contain border-none" 
+            loading="eager" 
+            draggable="false"
+        />
+    </div>
+    <div className="w-11/12 md:w-4/5 flex flex-col md:flex-row justify-start mt-8 gap-2">
+        {/* Major Test Series Card */}
+        <div style={{ backgroundColor: "#1a2439" }} className="w-full max-w-sm h-64 mt-8 rounded-lg">
+            <div className="p-4">
+                <button className="text-xs p-2 text-white rounded-full bg-black flex items-center">
+                    <svg className="mr-2" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="none" viewBox="0 0 16 16">
+                        <path stroke="#D8DBDF" stroke-linecap="round" stroke-linejoin="round" stroke-miterlimit="10" d="M14.867 5.733V8.4c0 2.333-1.2 3.333-3 3.333h-6c-1.8 0-3-1-3-3.333V5.733c0-1.766.69-2.766 1.8-3.146.36-.127.762-.187 1.2-.187h6c.438 0 .84.06 1.2.187 1.11.38 1.8 1.38 1.8 3.146z"></path>
+                        <path stroke="#D8DBDF" stroke-linecap="round" stroke-linejoin="round" stroke-miterlimit="10" d="M6.867 7.075v-.84c0-1.038.783-1.467 1.74-.947l.771.416.77.417c.958.52.958 1.37 0 1.89l-.77.417-.77.417c-.958.52-1.741.096-1.741-.948v-.822z"></path>
+                        <path stroke="#D8DBDF" stroke-linecap="round" d="M2.867 5.067v0c-.92 0-1.667.746-1.667 1.666v3.482a3.333 3.333 0 003.334 3.333h6.518a1.815 1.815 0 001.815-1.815v0"></path>
+                    </svg>
+                    ONLINE TEST SERIES
+                </button>
+            </div>
+
+            <div className="px-4 flex flex-col ">
+                <h1 className="font-montserrat text-lg font-medium text-white">Major Test Series</h1>
+                <h1 className="text-gray-400 font-montserrat text-sm font-normal">All India Test Series</h1>
+            </div>
+
+            <div className="flex justify-center flex-col px-4 mt-4">
+                <div className="flex items-center">
+                    <svg xmlns="http://www.w3.org/2000/svg" height="20px" viewBox="0 -960 960 960" width="20px" fill="#FFFFFF">
+                        <path d="M389-267 195-460l51-52 143 143 325-324 51 51-376 375Z"/>
+                    </svg>
+                    <h1 className="text-gray-400 pl-2 font-montserrat text-xs font-normal">10+ Full Syllabus Tests</h1>
                 </div>
-                <div className="w-4/5 flex justify-start flex-row">
-                    <div style = {{backgroundColor: "#1a2439"}}className="w-80 h-64 mt-8 rounded-lg">
-                            <div className="p-4  ">
-                                <button className=" text-xs p-2 text-white rounded-full bg-black flex items">
-                                <svg className="mr-2"xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="none" viewBox="0 0 16 16"><path stroke="#D8DBDF" stroke-linecap="round" stroke-linejoin="round" stroke-miterlimit="10" d="M14.867 5.733V8.4c0 2.333-1.2 3.333-3 3.333h-6c-1.8 0-3-1-3-3.333V5.733c0-1.766.69-2.766 1.8-3.146.36-.127.762-.187 1.2-.187h6c.438 0 .84.06 1.2.187 1.11.38 1.8 1.38 1.8 3.146z"></path><path stroke="#D8DBDF" stroke-linecap="round" stroke-linejoin="round" stroke-miterlimit="10" d="M6.867 7.075v-.84c0-1.038.783-1.467 1.74-.947l.771.416.77.417c.958.52.958 1.37 0 1.89l-.77.417-.77.417c-.958.52-1.741.096-1.741-.948v-.822z"></path><path stroke="#D8DBDF" stroke-linecap="round" d="M2.867 5.067v0c-.92 0-1.667.746-1.667 1.666v3.482a3.333 3.333 0 003.334 3.333h6.518a1.815 1.815 0 001.815-1.815v0"></path></svg>
 
-                                    ONLINE TEST SERIES</button>
-                            </div>
-
-                            <div className="px-4 flex flex-col">
-                                <h1 className="font-montserrat text-lg font-medium text-white">Major Test Series</h1>
-                                <h1 style={{color:"#bdc3ce"}} className="font-montserrat text-sm font-normal">All India Test Series</h1>
-                            </div>
-
-                            <div className="flex jusitfy-center flex-col px-4 mt-4">
-                                    <div className="flex items-center">
-                                    <svg xmlns="http://www.w3.org/2000/svg" height="20px" viewBox="0 -960 960 960" width="20px" fill="#FFFFFF"><path d="M389-267 195-460l51-52 143 143 325-324 51 51-376 375Z"/></svg>
-                                    <h1 style={{color:"#bdc3ce"}} className="pl-2 font-montserrat text-xs font-normal">10+ Full Syllabus Tests</h1>
-                                    </div>
-
-                                    <div className="flex items-center">
-                                    <svg xmlns="http://www.w3.org/2000/svg" height="20px" viewBox="0 -960 960 960" width="20px" fill="#FFFFFF"><path d="M389-267 195-460l51-52 143 143 325-324 51 51-376 375Z"/></svg>
-                                    <h1 style={{color:"#bdc3ce"}} className="pl-2 font-montserrat text-xs font-normal">Detailed solutions to all questions</h1>
-                                    </div>
-                                    <div className="flex items-center">
-                                    <svg xmlns="http://www.w3.org/2000/svg" height="20px" viewBox="0 -960 960 960" width="20px" fill="#FFFFFF"><path d="M389-267 195-460l51-52 143 143 325-324 51 51-376 375Z"/></svg>
-                                    <h1 style={{color:"#bdc3ce"}} className="pl-2 font-montserrat text-xs font-normal">Exclusive ALLEN's Question Bank</h1>
-                                    </div>
-                            </div>
-                        <div className="flex justify-between px-4 mt-6">
-                            <h1 className="font-montserrat text-white font-semibold text-lg">  ₹4,499</h1>
-                            <div className="flex items-center">
-                                <h1 style={{ color: "rgb(63, 141, 253)" }} className="font-montserrat text-white font-semibold text-sm">Know More</h1>
-                                <svg style={{ color: "rgb(63, 141, 253)" }} 
-                                xmlns="http://www.w3.org/2000/svg" height="16px" viewBox="0 -960 960 960" width="20px" fill="#FFFFFF"><path d="m288-96-68-68 316-316-316-316 68-68 384 384L288-96Z"/></svg>
-                            </div>
-                        </div>
-
-                    </div>
-                    <div style = {{backgroundColor: "#1a2439"}}className="w-80 h-64 mt-8 rounded-lg ml-4">
-                            <div className="p-4  ">
-                                <button className=" text-xs p-2 text-white rounded-full bg-black flex items">
-                                <svg className="mr-2"xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="none" viewBox="0 0 16 16"><path stroke="#D8DBDF" stroke-linecap="round" stroke-linejoin="round" stroke-miterlimit="10" d="M14.867 5.733V8.4c0 2.333-1.2 3.333-3 3.333h-6c-1.8 0-3-1-3-3.333V5.733c0-1.766.69-2.766 1.8-3.146.36-.127.762-.187 1.2-.187h6c.438 0 .84.06 1.2.187 1.11.38 1.8 1.38 1.8 3.146z"></path><path stroke="#D8DBDF" stroke-linecap="round" stroke-linejoin="round" stroke-miterlimit="10" d="M6.867 7.075v-.84c0-1.038.783-1.467 1.74-.947l.771.416.77.417c.958.52.958 1.37 0 1.89l-.77.417-.77.417c-.958.52-1.741.096-1.741-.948v-.822z"></path><path stroke="#D8DBDF" stroke-linecap="round" d="M2.867 5.067v0c-.92 0-1.667.746-1.667 1.666v3.482a3.333 3.333 0 003.334 3.333h6.518a1.815 1.815 0 001.815-1.815v0"></path></svg>
-
-                                    ONLINE TEST SERIES</button>
-                            </div>
-
-                            <div className="px-4 flex flex-col">
-                                <h1 className="font-montserrat text-lg font-medium text-white">Leader Test Series</h1>
-                                <h1 style={{color:"#bdc3ce"}} className="font-montserrat text-sm font-normal">All India Test Series</h1>
-                            </div>
-
-                            <div className="flex jusitfy-center flex-col px-4 mt-4">
-                                    <div className="flex items-center">
-                                    <svg xmlns="http://www.w3.org/2000/svg" height="20px" viewBox="0 -960 960 960" width="20px" fill="#FFFFFF"><path d="M389-267 195-460l51-52 143 143 325-324 51 51-376 375Z"/></svg>
-                                    <h1 style={{color:"#bdc3ce"}} className="pl-2 font-montserrat text-xs font-normal">20+ (Part+Full) Syllabus Tests</h1>
-                                    </div>
-
-                                    <div className="flex items-center">
-                                    <svg xmlns="http://www.w3.org/2000/svg" height="20px" viewBox="0 -960 960 960" width="20px" fill="#FFFFFF"><path d="M389-267 195-460l51-52 143 143 325-324 51 51-376 375Z"/></svg>
-                                    <h1 style={{color:"#bdc3ce"}} className="pl-2 font-montserrat text-xs font-normal">Detailed solutions to all questions</h1>
-                                    </div>
-                                    <div className="flex items-center">
-                                    <svg xmlns="http://www.w3.org/2000/svg" height="20px" viewBox="0 -960 960 960" width="20px" fill="#FFFFFF"><path d="M389-267 195-460l51-52 143 143 325-324 51 51-376 375Z"/></svg>
-                                    <h1 style={{color:"#bdc3ce"}} className="pl-2 font-montserrat text-xs font-normal">Exclusive ALLEN's Question Bank</h1>
-                                    </div>
-                            </div>
-                        <div className="flex justify-between px-4 mt-6">
-                            <h1 className="font-montserrat text-white font-semibold text-lg">  ₹5,999</h1>
-                            <div className="flex items-center">
-                                <h1 style={{ color: "rgb(63, 141, 253)" }} className="font-montserrat text-white font-semibold text-sm">Know More</h1>
-                                <svg style={{ color: "rgb(63, 141, 253)" }} 
-                                xmlns="http://www.w3.org/2000/svg" height="16px" viewBox="0 -960 960 960" width="20px" fill="#FFFFFF"><path d="m288-96-68-68 316-316-316-316 68-68 384 384L288-96Z"/></svg>
-                            </div>
-                        </div>
-
-                    </div>
+                <div className="flex items-center">
+                    <svg xmlns="http://www.w3.org/2000/svg" height="20px" viewBox="0 -960 960 960" width="20px" fill="#FFFFFF">
+                        <path d="M389-267 195-460l51-52 143 143 325-324 51 51-376 375Z"/>
+                    </svg>
+                    <h1 className="text-gray-400 pl-2 font-montserrat text-xs font-normal">Detailed solutions to all questions</h1>
+                </div>
+                <div className="flex items-center">
+                    <svg xmlns="http://www.w3.org/2000/svg" height="20px" viewBox="0 -960 960 960" width="20px" fill="#FFFFFF">
+                        <path d="M389-267 195-460l51-52 143 143 325-324 51 51-376 375Z"/>
+                    </svg>
+                    <h1 className="text-gray-400 pl-2 font-montserrat text-xs font-normal">Exclusive ALLEN's Question Bank</h1>
                 </div>
             </div>
+            <div className="flex justify-between px-4 mt-6">
+                <h1 className="font-montserrat text-white font-semibold text-lg">₹4,499</h1>
+                <div className="flex items-center">
+                    <h1 className="text-blue-400 font-montserrat text-white font-semibold text-sm">Know More</h1>
+                    <svg className="text-blue-400" xmlns="http://www.w3.org/2000/svg" height="16px" viewBox="0 -960 960 960" width="20px" fill="#FFFFFF">
+                        <path d="m288-96-68-68 316-316-316-316 68-68 384 384L288-96Z"/>
+                    </svg>
+                </div>
+            </div>
+        </div>
+        
+       
+        <div style={{ backgroundColor: "#1a2439" }} className="w-full max-w-sm h-64 mt-8 rounded-lg ">
+            <div className="p-4">
+                <button className="text-xs p-2 text-white rounded-full bg-black flex items-center">
+                    <svg className="mr-2" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="none" viewBox="0 0 16 16">
+                        <path stroke="#D8DBDF" stroke-linecap="round" stroke-linejoin="round" stroke-miterlimit="10" d="M14.867 5.733V8.4c0 2.333-1.2 3.333-3 3.333h-6c-1.8 0-3-1-3-3.333V5.733c0-1.766.69-2.766 1.8-3.146.36-.127.762-.187 1.2-.187h6c.438 0 .84.06 1.2.187 1.11.38 1.8 1.38 1.8 3.146z"></path>
+                        <path stroke="#D8DBDF" stroke-linecap="round" stroke-linejoin="round" stroke-miterlimit="10" d="M6.867 7.075v-.84c0-1.038.783-1.467 1.74-.947l.771.416.77.417c.958.52.958 1.37 0 1.89l-.77.417-.77.417c-.958.52-1.741.096-1.741-.948v-.822z"></path>
+                        <path stroke="#D8DBDF" stroke-linecap="round" d="M2.867 5.067v0c-.92 0-1.667.746-1.667 1.666v3.482a3.333 3.333 0 003.334 3.333h6.518a1.815 1.815 0 001.815-1.815v0"></path>
+                    </svg>
+                    ONLINE TEST SERIES
+                </button>
+            </div>
+
+            <div className="px-4 flex flex-col">
+                <h1 className="font-montserrat text-lg font-medium text-white">Leader Test Series</h1>
+                <h1 className="text-gray-400 font-montserrat text-sm font-normal">All India Test Series</h1>
+            </div>
+
+            <div className="flex justify-center flex-col px-4 mt-4">
+                <div className="flex items-center">
+                    <svg xmlns="http://www.w3.org/2000/svg" height="20px" viewBox="0 -960 960 960" width="20px" fill="#FFFFFF">
+                        <path d="M389-267 195-460l51-52 143 143 325-324 51 51-376 375Z"/>
+                    </svg>
+                    <h1 className="text-gray-400 pl-2 font-montserrat text-xs font-normal">10+ Full Syllabus Tests</h1>
+                </div>
+
+                <div className="flex items-center">
+                    <svg xmlns="http://www.w3.org/2000/svg" height="20px" viewBox="0 -960 960 960" width="20px" fill="#FFFFFF">
+                        <path d="M389-267 195-460l51-52 143 143 325-324 51 51-376 375Z"/>
+                    </svg>
+                    <h1 className="text-gray-400 pl-2 font-montserrat text-xs font-normal">Detailed solutions to all questions</h1>
+                </div>
+                <div className="flex items-center">
+                    <svg xmlns="http://www.w3.org/2000/svg" height="20px" viewBox="0 -960 960 960" width="20px" fill="#FFFFFF">
+                        <path d="M389-267 195-460l51-52 143 143 325-324 51 51-376 375Z"/>
+                    </svg>
+                    <h1 className="text-gray-400 pl-2 font-montserrat text-xs font-normal">Exclusive ALLEN's Question Bank</h1>
+                </div>
+            </div>
+            <div className="flex justify-between px-4 mt-6">
+                <h1 className="font-montserrat text-white font-semibold text-lg">₹4,499</h1>
+                <div className="flex items-center">
+                    <h1 className="text-blue-400 font-montserrat text-white font-semibold text-sm">Know More</h1>
+                    <svg className="text-blue-400" xmlns="http://www.w3.org/2000/svg" height="16px" viewBox="0 -960 960 960" width="20px" fill="#FFFFFF">
+                        <path d="m288-96-68-68 316-316-316-316 68-68 384 384L288-96Z"/>
+                    </svg>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
             <HeaderAndCard imageUrl="https://res.cloudinary.com/dpzpn3dkw/image/upload/v1729148776/school_marks_ancrd5.webp" headerTitle="Practice Package" type="PRACTICE" cardTitle1="Nurture Practice Packege" cardTitle2="Allen Question Bank" description1="15k+ curated questions covering all topics " description2="DPPs, PYQs & more" description3="Exclusive ALLEN's Question Bank" price="1,999"/>
 
             
